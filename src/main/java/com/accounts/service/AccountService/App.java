9bdd545577c18ccdb00ea3da82f9a8bf3c7fad0e
tanaysh7 <tanaysh7@gmail.com>
2017-09-17 06:58:19 +0530

    set up done

--- src/main/java/com/accounts/service/AccountService/App.java
+++ src/main/java/com/accounts/service/AccountService/App.java
@@ -1,5 +1,4 @@
 package com.accounts.service.AccountService;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
